324f20ab6a31dc65cb7df68d938e43e1bee4552a
sbsudip <63397418+sbsudip@users.noreply.github.com>
2022-06-15 16:56:52 +0545

    First Commit

---
+++ src/Component/forms.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { useState } from 'react';
+
+export default function OtherInfo() {
+    const [firstName, setFirstName] = useState(""),
+        [lastName, setLastName] = useState(""),
+        [age, setAge] = useState(1),
+        [submit, setSubmit] = useState(false)
+        ;
+    function SubmitButton(e) {
+        e.preventDefault()
+        setSubmit(true);
+
+    }
+ 
+    return <div className='row'>
+        <form className='column-6'>
+            <div className=''>
+                <div className='form-group'>
+                    <label>First Name :</label>
+                    <input type="text" id="firstName" placeholder='First Name Please' value={firstName !== "" ? firstName : ""} onChange={(e) => setFirstName(e.target.value)}></input>
+                </div>
+                <div className='form-group'>
+                    <label>Last Name :</label>
+                    <input type="text" id="lastName" placeholder='Last Name Please' value={lastName !== "" ? lastName : ""} onChange={(e) => setLastName(e.target.value)}></input>
+                </div>
+                <div className='form-group'>
+                    <label >Your Age :</label>
+                    <input type="number" id="age" min="1" placeholder='Enter Age' value={age} onChange={(e) => setAge(e.target.value)}></input>
+                </div>
+                <div className='form-group center'>
+                    <button className='btn btn-success m-3' onClick={(e) => SubmitButton(e)}>Submit</button>
+                </div>
+            </div>
+        </form>
+        {submit === true &&
+            <div className='Righthand'>
+                <div className='confirm'>
+                    <h3>Detail About You</h3>
+                    Nice to meet you,{firstName} {lastName}, You are {age} years old..
+                </div>
+            </div>}
+    </div>
+}
